b19051813b065ce36bab3d79cb223534f3c3fef3
Bilal Sadiq <bnaq860@gmail.com>
2026-02-15 12:12:20 +0530

    add initial project setup and db connection

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import { app } from "./app";
+
+const start = async () => {
+  try {
+    await app.listen({ port: 4000, host: "0.0.0.0" });
+    console.log("Backend running on http://localhost:4000");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
